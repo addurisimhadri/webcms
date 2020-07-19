a1217f29348112b3bbb66d9d711a3d1c99446459
addurisimhadri <adduriappanna@gmail.com>
2020-07-19 22:09:18 +0530

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MymaterialModule } from '../mymaterial/mymaterial.module';
+import {FlexLayoutModule} from '@angular/flex-layout'
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [    
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MymaterialModule,
+    FlexLayoutModule,
+    RouterModule
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
